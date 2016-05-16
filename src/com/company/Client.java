111cb11ee134a7d99db05549f117c77699a4438d
cmauer69 <cmauer@mauerenterprises.com>
2016-05-16 15:09:50 -0500

    Second upload

---
+++ src/com/company/Client.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by clifford.mauer on 5/16/2016.
+ */
+public class Client {
+}
---
+++ test/testClient.java
@@ -0,0 +1,5 @@
+/**
+ * Created by clifford.mauer on 5/16/2016.
+ */
+public class testClient {
+}
